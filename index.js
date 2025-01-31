834c9a75ac35e63a18cf253af2bb57ce4b2dcd04
Cem Renkligül <cem.renkliguel@outlook.de>
2025-01-31 16:34:59 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function answer(response) {
-    document.getElementById("responseMessage").innerText = "Du hast geantwortet: " + response;
-    console.log("Antworten:", response);
-}
+    document.getElementById("responseMessage").innerText =
+      "Du hast geantwortet: " + response;
+  
+    // Google Formular URL + korrekte Feld-ID
+    fetch(
+      "https://docs.google.com/forms/d/e/1FAIpQLSckJhPA7GEw4R0E6ZhKs46g7STafkQW87yJ3AMY9UHnxeczZg/formResponse",
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `entry.1490941661=${encodeURIComponent(response)}`,
+      }
+    ).then(() => console.log("Antwort gesendet:", response));
+  }
+  
